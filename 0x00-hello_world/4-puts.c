d4686e1d3341328bb96829c076668d5678bf32b3
Mamba1990 <h.daoudim@outlook.com>
2023-04-20 15:33:03 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- * main: the main function prints a message using puts
+ * main - Prints a message using puts
  *
  * Return: 0
  */
 int main (void)
 {
-         puts("\" Programming is like building a multilingual puzzle");
+         puts("\"Programming is like building a multilingual puzzle");
 	 return(0);
 }
 
