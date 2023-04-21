aa2116e581ea2fd0e6d1e318c5cd1050f7d6e729
Mamba1990 <h.daoudim@outlook.com>
2023-04-21 16:14:57 -0700

    tsk4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+* Return: 0
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 111)
+		{
+			n++;
+			continue
+		}
+		putchar(n);
+		n++;
+	}
+	
+        putchar('\n');
+
+        return (0);
+}
