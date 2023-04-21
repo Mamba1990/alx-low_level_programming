ba7d8a58234b029ac4eaa6bdb23907ad4a7057a7
Mamba1990 <h.daoudim@outlook.com>
2023-04-20 17:20:34 -0700

    right

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
-/**
-* main -Prints a message using puts
-*
-* Return: 0
-*/
-
+ #include <stdio.h>
+ /**
+ * main -Prints a message using puts
+ *
+ * Return: 0
+ */
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
 
