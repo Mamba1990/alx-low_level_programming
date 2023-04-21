f590df95a1de378907d44a1eaf4acc4c5f93ee82
Mamba1990 <h.daoudim@outlook.com>
2023-04-21 14:38:15 -0700

    task03

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+* Return: 0
+*/
+int main(void)
+{
+	char lower_case;
+
+	for(lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar('\n');
+	return (0);
+}
