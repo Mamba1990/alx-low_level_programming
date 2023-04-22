5cf8c020783a188bf649ba186e9db593226b117e
Mamba1990 <h.daoudim@outlook.com>
2023-04-21 17:29:10 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)	
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
