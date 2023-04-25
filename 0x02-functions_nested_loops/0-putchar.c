0c0bd8f418145ff266d33e21264561db31d8bc2a
Mamba1990 <h.daoudim@outlook.com>
2023-04-25 02:37:32 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+* Return: 0
+*/
+int main(void)
+{
+	char c='h';
+	putchar(_putchar('h'));
+	putchar('\n');
+	return (0);
+}
