37bd833cb60faad9f6c17eb8810ef8c286130d9b
Mamba1990 <h.daoudim@outlook.com>
2023-04-25 06:23:30 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+*print_alphabet_x10 - prints alphabet 10 times in lowercase
+*Description: 'the program's description'
+*Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		char lower_case;
+
+		for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+		{
+			_putchar(lower_case);
+		}
+	}
+	_putchar('\n');
+}
