293df2e033cff87a8b7356dbe45232f1560cc4e1
Mamba1990 <h.daoudim@outlook.com>
2023-04-25 17:09:37 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,37 @@
 */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
-		for (i = n; i > 98; i--)
-		{
-			printf("%d, ", i);
-		}
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
 	else
 	{
-		for (i = n; i < 98; i++)
-		{
-			printf("%d, ", i);
-		}
-	 printf("98\n");
+	printf("%d, ", n);
 	}
-_putchar('\n');
+	}
+	}
+	else
+	{
+	for (; n <= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
 }
+
