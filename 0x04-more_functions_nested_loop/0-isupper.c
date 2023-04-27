a78552e4801b0b90be5f2540b376d6cb40c2f05d
Mamba1990 <h.daoudim@outlook.com>
2023-04-27 06:57:41 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loop/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - Checks for uppercase character
+*@c: The character is in ASCII code
+*
+*Description: 'the program's description'
+*Return: 0
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
