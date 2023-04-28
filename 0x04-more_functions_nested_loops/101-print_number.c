d27b9f6020b6e2456a7f6bf01d6a287e0bc01e49
Mamba1990 <h.daoudim@outook.com>
2023-04-28 15:01:37 -0700

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: no retrun
+*/
+void print_number(int n)
+{
+	unsigned int n_1;
+
+	if (n < 0)
+	{
+		n_1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n_1 = n;
+	}
+	if (n_1 / 10)
+	{
+		print_number(n_1 / 10);
+	}
+	_putchar((n_1 % 10) + '0');
+}
+
+
+
