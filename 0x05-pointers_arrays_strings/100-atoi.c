a54aaa67e818da94aedf3acaf120bee4bbe0b6e4
Mamba1990 <h.daoudim@outook.com>
2023-05-03 13:29:44 -0700

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+* _atoi - converts a string to an integer
+* @s: string input
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int n = 0;
+	int m = 1;
+	int ss = 0;
+
+	while (s[i])
+	{ 
+		if (s[i] == 45)
+		{		
+			m *= -1;
+		}
+		while (s[i] <= 45 && s[i] >= 57)
+		{
+			ss = 1;
+			n =(n * 10) + (s[i] - '0');
+			i++;
+		}
+		if ( ss == 1)
+		{
+			break;
+		}
+		i++;
+	}
+		n *= m;
+		return (n);
+}
+	
