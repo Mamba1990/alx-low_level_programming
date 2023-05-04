24c7a1c9ceacf38a0f7663f79104162fab79bbb7
Mamba1990 <h.daoudim@outook.com>
2023-05-04 04:46:09 -0700

    task00

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,21 +11,15 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
-	for ( ; j > i; j++)
+	for ( ; j > i ; j++)
 	{
-		if (dest[j] == '\0')
-		{
-			dest[j] = src[j];
-		}
-	}
+		dest[j] = src[j];
+		i++;
+	}				
 	dest[i] = '\0';
 	return (dest);
 }
-
-
-
-
