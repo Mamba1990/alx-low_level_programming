a34e579e53d77eede0c7757f25e14237b4590b05
Mamba1990 <h.daoudim@outook.com>
2023-05-08 06:43:31 -0700

    02-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
  * _strchr -  locates a character in a string.
  * @s: string input
@@ -20,7 +20,7 @@ char *_strchr(char *s, char c)
 		}
 		j++;
 	}
-return (NULL);
+return (0);
 }
 
 
