47b1c77caf50f00a21a519ee9e7672a5f7581af0
Mamba1990 <h.daoudim@outook.com>
2023-05-12 10:28:00 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point.
+ * @argc: argument count
+ * @argv: argument vector (one dim array)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
