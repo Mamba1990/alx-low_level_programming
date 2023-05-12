1020b79863d4f9d0df8b9aed361478871e72ef7a
Mamba1990 <h.daoudim@outook.com>
2023-05-12 12:49:37 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+int atoi(char *s);
+/**
+ * main - Entry point.
+ * @argc: argument count
+ * @argv: argument vector (one dim array)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, mul;
+
+	a = 0;
+	b = 0;
+	mul = 0;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", mul = a * b);
+	}
+	else
+	{
+		printf("Eerror\n");
+		return (1);
+	}
+
+return (0);
+
+}
