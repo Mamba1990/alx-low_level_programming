dcde2e8d7efa58d498a344b8f2cfa3f81cd804e0
Mamba1990 <h.daoudim@outook.com>
2023-05-12 14:13:03 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point.
+ * @argc: argument count
+ * @argv: argument vector (one dim array)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned j, sum;
+	char *c;
+	
+	sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			c = argv[i];
+		for (j = 0;j < strlen(c); j++)
+		{
+			if (c[j] < 48 || c[j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(c);
+		c++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+return (0);
+}
+
