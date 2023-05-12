701523566954988f73555983d5f47d140ac44262
Mamba1990 <h.daoudim@outook.com>
2023-05-12 09:24:31 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: argument count
+ * @argv: argument vector (one dim array)
+ *
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	argc = 0;
+
+	printf ("This program name is \"%s\".\n",argv[argc]);
+return (0);
+}
