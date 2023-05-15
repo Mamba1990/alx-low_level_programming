949f333aa351e24d55e0cab78c3ecdc6c75e7089
Mamba1990 <h.daoudim@outook.com>
2023-05-15 16:50:34 -0700

    04

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	mem = malloc(sizeof(int *) * height);
+	mem = malloc(sizeof(int*) * height);
 	if (mem == NULL)
 	{
 		free(mem);
