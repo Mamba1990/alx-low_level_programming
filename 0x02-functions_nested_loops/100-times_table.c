aa4f9957bdbcf4514c2d9ae68b2e94d4ca6f5a45
Mamba1990 <h.daoudim@outook.com>
2023-05-21 15:41:43 -0700

    0102-fibonacci.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_times_table - prints the n times table, starting with 0
+*
+* Description: 'the program's description'
+* Return: 0
+*/
+void print_times_table(int n)
+{
+
