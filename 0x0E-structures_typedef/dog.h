47ac5029b7d8345e68cf81c38c5835941903ad11
Mamba1990 <h.daoudim@outook.com>
2023-05-22 10:37:17 -0700

    0-dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog's ID
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
