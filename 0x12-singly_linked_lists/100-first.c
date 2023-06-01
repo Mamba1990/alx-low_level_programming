f3eabc0d2226b0625f5e4236e25636d9305b9b9d
Mamba1990 <h.daoudim@outlook.com>
2023-06-01 11:48:12 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void print_first(void);
+
+/**
+ * print_firs - prints a message before the main
+ *
+ * Return: nothing
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
