388a03af4bcfd615dcb1fc4b5076adfa5076c75c
Mamba1990 <h.daoudim@outlook.com>
2023-06-06 08:21:31 -0700

    delete_node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *t = *head;
 	listint_t *node = NULL;
-	unsigned int j;
+	unsigned int j = 0;
 
 	if (*head == NULL)
 	{
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (j = 0; j < index - 1; j++)
+	for ( ; j < index - 1; j++)
 	{
 		if (!t || !(t->next))
 		{
