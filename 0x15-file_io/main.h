e8893b3bc3c2d35704ebd83e830fb22f0813c490
Mamba1990 <h.daoudim@outlook.com>
2023-07-10 05:59:54 -0700

    00-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,16 @@
-#include <elf.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-void elfChecker(unsigned char *e_ident);
-void magicPrinter(unsigned char *e_ident);
-void classPrinter(unsigned char *e_ident);
-void dataPrinter(unsigned char *e_ident);
-void versionPrinter(unsigned char *e_ident);
-void abiPrinter(unsigned char *e_ident);
-void osabiPrinter(unsigned char *e_ident);
-void typePrinter(unsigned int e_type, unsigned char *e_ident);
-void entryPrinter(unsigned long int e_entry, unsigned char *e_ident);
-void elfClose(int elf);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+
+#endif
+
