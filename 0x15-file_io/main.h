2f2d16a197a0c3b676c4b6dd2a8e211232023144
Mamba1990 <h.daoudim@outlook.com>
2023-07-10 06:46:19 -0700

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,16 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void elfChecker(unsigned char *e_ident);
+void magicPrinter(unsigned char *e_ident);
+void classPrinter(unsigned char *e_ident);
+void dataPrinter(unsigned char *e_ident);
+void versionPrinter(unsigned char *e_ident);
+void abiPrinter(unsigned char *e_ident);
+void osabiPrinter(unsigned char *e_ident);
+void typePrinter(unsigned int e_type, unsigned char *e_ident);
+void entryPrinter(unsigned long int e_entry, unsigned char *e_ident);
+void elfClose(int elf);
 
 
 #endif
