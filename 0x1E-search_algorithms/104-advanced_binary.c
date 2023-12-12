347728e38eba6e09acb8151a6f9e496ed59f4c89
Mamba1990 <h.daoudim@outlook.com>
2023-12-12 10:24:49 -0800

    104

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,61 @@
+#include "search_algos.h"
+
+/**
+ * recSarch - searches for a value in an array of
+ * ints using the binary SA
+ * @array: input
+ * @size: array's size
+ * @value: value to be searched
+ * Return: idx of the number
+ */
+int recSearch(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (recSearch(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (recSearch(array, half + 1, value));
+
+	half++;
+	return (recSearch(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - calling to recSearch to return
+ * the index of the number
+ * @array: inpu
+ * @size: the array's size
+ * @value: value to be  searched
+ * Return: index of the number
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int idx;
+
+	idx = recSearch(array, size, value);
+
+	if (idx >= 0 && array[idx] != value)
+		return (-1);
+
+	return (idx);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,5 @@ void print_array(int *array, size_t l, size_t r);
 int binary_search_index(int *array, size_t l, size_t r, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif 
